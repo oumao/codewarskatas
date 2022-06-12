45c404073e78e10df3092ad4ee7e3d4f9d9b1e3f
oumao <evansotis2015@gmail.com>
2022-06-12 15:38:54 +0300

    Finding pair of gloves In a random box O(n)

--- pair_gloves.py
+++ pair_gloves.py
@@ -1,3 +1,7 @@
 from collections import Counter
 def find_pair(gloves):
-    res = len([(key, key) for key, val in Counter(gloves).items() for _ in range(val//2)])
+    return len([(key, key) for key, val in Counter(gloves).items() for _ in range(val//2)])
+
+
+print(find_pair(["gray","black","purple","purple","gray","black"]))
+print(find_pair(["White","Yellow","Fuchsia","Red","Silver","Gray","Olive","Purple", "Navy", "Lime", "Maroon","Aqua","Lime","Teal","Green","Blue","Navy","Black"]))
